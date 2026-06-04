b47a2b4a536bb28803aec6ab4d291b11cfe70d96
baran sagis <61687681+baransagis@users.noreply.github.com>
2026-06-04 14:36:58 +0200

    deleted unused files

--- shared/src/androidMain/kotlin/eu/baran/pokedex/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package eu.baran.pokedex
-
-import android.os.Build
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
